dffd64d8f681e7e67eb94fd75c15d09adcbb1bcb
VinodReddem <vinod.reddem@gmail.com>
2021-04-27 21:47:12 +0530

    Changes in Connection file

--- user_data/database_connection.py
+++ user_data/database_connection.py
@@ -1,6 +1,11 @@
+import os
 from google.cloud import firestore
+from firebase_admin import credentials, initialize_app
 
 #Initialize an instance of Firestore
-db = firestore.Client()
+data = os.path.abspath(os.path.dirname(__file__)) + "/key.json"
+cred = credentials.Certificate(data)
+default_app = initialize_app(cred)
 
+db = firestore.Client()
 doc_collection = db.collection(u'counting')
